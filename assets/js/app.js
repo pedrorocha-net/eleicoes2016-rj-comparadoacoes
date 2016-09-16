76cc5701ad3734bfd3ceec601b479217754e9e62
Pedro Rocha <pedrogomesrocha@gmail.com>
2016-09-16 03:21:57 -0300

    Update 2016-09-16T06:21:55.096Z

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,9 +1,39 @@
 "use strict";
-this.App = angular.module("app", []);
+this.App = angular.module("app", ['templates']);
 
 this.App.controller('PageController', function($scope, $http) {
   return $http.get('https://raw.githubusercontent.com/pedrorocha-net/' + 'eleicoes2016-rj-comparadoacoes/master/data/candidatos.json').then(function(result) {
     $scope.candidatos = result.data;
-    return console.log($scope.candidatos);
+    $scope.selecao1 = null;
+    $scope.selecao2 = null;
+    return $scope.selecionar = function(candidato) {
+      var ref, ref1;
+      if (((ref = $scope.selecao1) != null ? ref.id : void 0) === candidato.id) {
+        $scope.selecao1 = null;
+        return;
+      }
+      if (((ref1 = $scope.selecao2) != null ? ref1.id : void 0) === candidato.id) {
+        $scope.selecao2 = null;
+        return;
+      }
+      if (!($scope != null ? $scope.selecao1 : void 0)) {
+        $scope.selecao1 = candidato;
+        return;
+      }
+      if (!(($scope != null ? $scope.selecao1 : void 0) && ($scope != null ? $scope.selecao2 : void 0))) {
+        $scope.selecao2 = candidato;
+      }
+    };
   });
+});
+
+this.App.directive('card', function() {
+  return {
+    restrict: 'A',
+    scope: {
+      ngCandidato: '='
+    },
+    templateUrl: 'assets/js/card.html',
+    link: function($scope, element, attrs) {}
+  };
 });
