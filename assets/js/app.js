32988cef54afe6b9de77924dba25174410866087
Pedro Rocha <pedrogomesrocha@gmail.com>
2016-09-16 03:39:05 -0300

    Update 2016-09-16T06:39:00.941Z

--- assets/js/app.js
+++ assets/js/app.js
@@ -6,8 +6,9 @@ this.App.controller('PageController', function($scope, $http) {
     $scope.candidatos = result.data;
     $scope.selecao1 = null;
     $scope.selecao2 = null;
+    $scope.selecao3 = null;
     return $scope.selecionar = function(candidato) {
-      var ref, ref1;
+      var ref, ref1, ref2;
       if (((ref = $scope.selecao1) != null ? ref.id : void 0) === candidato.id) {
         $scope.selecao1 = null;
         return;
@@ -16,12 +17,20 @@ this.App.controller('PageController', function($scope, $http) {
         $scope.selecao2 = null;
         return;
       }
+      if (((ref2 = $scope.selecao3) != null ? ref2.id : void 0) === candidato.id) {
+        $scope.selecao3 = null;
+        return;
+      }
       if (!($scope != null ? $scope.selecao1 : void 0)) {
         $scope.selecao1 = candidato;
         return;
       }
       if (!(($scope != null ? $scope.selecao1 : void 0) && ($scope != null ? $scope.selecao2 : void 0))) {
         $scope.selecao2 = candidato;
+        return;
+      }
+      if (!(($scope != null ? $scope.selecao1 : void 0) && ($scope != null ? $scope.selecao2 : void 0) && ($scope != null ? $scope.selecao3 : void 0))) {
+        $scope.selecao3 = candidato;
       }
     };
   });
