4c139007f19e29222bf490ea3f8d0d4390453426
Pedro Rocha <pedrogomesrocha@gmail.com>
2016-09-29 14:15:43 -0300

    Atualizando JSON

--- data/crawler/generate.php
+++ data/crawler/generate.php
@@ -5,86 +5,84 @@ include_once './Cidade.php';
 include_once './Candidato.php';
 include_once './Doacao.php';
 
-$arquivo_processado = 'candidatos_dados_processados.json';
+$config = file_get_contents('../config.json');
 
-/**
- * Para alterar para outras cidades, leia as intruções em
- * https://github.com/pedrorocha-net/eleicoes2016-rj-comparadoacoes
- */
-$ano = 2016;
-$cidade_id = 60011; // Rio de Janeiro - RJ
-$eleicao_id = 2; // 2016
-$cargo_id = 11; // Prefeito
-$Cidade = new Cidade($ano, $cidade_id, $eleicao_id, $cargo_id);
+foreach ($config->cidades as $cidade) {
 
-// Apagar conteúdo sobre candidatos
-file_put_contents('../' . $arquivo_processado, '');
+  $Cidade = new Cidade($config->ano, $cidade->id, $config->eleicao_id, $config->cargo_id);
 
-$candidatos_novo = [];
+  $id_json = $config->ano . $config->eleicao_id . $cidade->id . $config->cargo_id;
+  $arquivo_processado = 'candidatos_dados_processados_' . $id_json . '.json';
 
-foreach ($Cidade->candidatos as $candidato_obj) {
-  $Candidato = new Candidato($ano, $cidade_id, $eleicao_id, $candidato_obj->id);
+  // Apagar conteúdo sobre candidatos
+  file_put_contents('../' . $arquivo_processado, '');
 
-  $item_candidato = [];
-  $item_candidato['id'] = $candidato_obj->id;
-  $item_candidato['nome'] = $candidato_obj->nomeUrna;
-  $item_candidato['numero'] = $candidato_obj->numero;
-  $item_candidato['partido'] = $candidato_obj->partido;
-  $item_candidato['slogan'] = $candidato_obj->nomeColigacao;
-  $item_candidato['contribuicoesEstimadasTotal'] = 0;
-  $item_candidato['contribuicoesEstimadasQtd'] = 0;
-  $item_candidato['contribuicoesFinanceirasPFTotal'] = 0;
-  $item_candidato['contribuicoesFinanceirasPFQtd'] = 0;
-  $item_candidato['fundoPartidario'] = 0;
-  $item_candidato['fundoPartidarioQtd'] = 0;
-  $receitas = $Candidato->getReceitas();
+  $candidatos_novo = [];
 
-  $doacoes = [];
-  foreach ($receitas as $receita) {
-    $Doacao = new Doacao($receita);
-    $doacao_formatada = $Doacao->formatar();
-    if ($doacao_formatada['especieRecurso'] != 'Estimado') {
-      if ($doacao_formatada['fonteOrigem'] == 'Fundo Partidário') {
-        $item_candidato['fundoPartidario'] += $doacao_formatada['valorReceita'];
-        $item_candidato['fundoPartidarioQtd']++;
-      }
-      else {
-        $item_candidato['contribuicoesFinanceirasPFQtd']++;
-        $item_candidato['contribuicoesFinanceirasPFTotal'] += $doacao_formatada['valorReceita'];
+  foreach ($Cidade->candidatos as $candidato_obj) {
+    $Candidato = new Candidato($config->ano, $cidade->id, $config->eleicao_id, $candidato_obj->id);
 
-        if (isset($doacoes[$doacao_formatada['nomeDoador']])) {
-          $doacoes[$doacao_formatada['nomeDoador']]['valorReceita'] += $doacao_formatada['valorReceita'];
+    $item_candidato = [];
+    $item_candidato['id'] = $candidato_obj->id;
+    $item_candidato['nome'] = $candidato_obj->nomeUrna;
+    $item_candidato['numero'] = $candidato_obj->numero;
+    $item_candidato['partido'] = $candidato_obj->partido;
+    $item_candidato['slogan'] = $candidato_obj->nomeColigacao;
+    $item_candidato['contribuicoesEstimadasTotal'] = 0;
+    $item_candidato['contribuicoesEstimadasQtd'] = 0;
+    $item_candidato['contribuicoesFinanceirasPFTotal'] = 0;
+    $item_candidato['contribuicoesFinanceirasPFQtd'] = 0;
+    $item_candidato['fundoPartidario'] = 0;
+    $item_candidato['fundoPartidarioQtd'] = 0;
+    $receitas = $Candidato->getReceitas();
+
+    $doacoes = [];
+    foreach ($receitas as $receita) {
+      $Doacao = new Doacao($receita);
+      $doacao_formatada = $Doacao->formatar();
+      if ($doacao_formatada['especieRecurso'] != 'Estimado') {
+        if ($doacao_formatada['fonteOrigem'] == 'Fundo Partidário') {
+          $item_candidato['fundoPartidario'] += $doacao_formatada['valorReceita'];
+          $item_candidato['fundoPartidarioQtd']++;
         }
         else {
-          $doacoes[$doacao_formatada['nomeDoador']] = $doacao_formatada;
+          $item_candidato['contribuicoesFinanceirasPFQtd']++;
+          $item_candidato['contribuicoesFinanceirasPFTotal'] += $doacao_formatada['valorReceita'];
+
+          if (isset($doacoes[$doacao_formatada['nomeDoador']])) {
+            $doacoes[$doacao_formatada['nomeDoador']]['valorReceita'] += $doacao_formatada['valorReceita'];
+          }
+          else {
+            $doacoes[$doacao_formatada['nomeDoador']] = $doacao_formatada;
+          }
         }
       }
+      else {
+        $item_candidato['contribuicoesEstimadasTotal'] += $doacao_formatada['valorReceita'];
+        $item_candidato['contribuicoesEstimadasQtd']++;
+      }
+    }
+
+    if ($item_candidato['contribuicoesFinanceirasPFQtd'] > 0) {
+      $item_candidato['apoioMedio'] = $item_candidato['contribuicoesFinanceirasPFTotal'] / $item_candidato['contribuicoesFinanceirasPFQtd'];
     }
     else {
-      $item_candidato['contribuicoesEstimadasTotal'] += $doacao_formatada['valorReceita'];
-      $item_candidato['contribuicoesEstimadasQtd']++;
+      $item_candidato['apoioMedio'] = $item_candidato['contribuicoesFinanceirasPFTotal'];
     }
+
+    $doacoes = array_values($doacoes);
+
+    usort($doacoes, function ($a, $b) {
+      return $b['valorReceita'] - $a['valorReceita'];
+    });
+
+    $item_candidato['maioresApoiadores'] = array_slice($doacoes, 0, 10);
+    $candidatos_novo[] = $item_candidato;
   }
 
-  if ($item_candidato['contribuicoesFinanceirasPFQtd'] > 0) {
-    $item_candidato['apoioMedio'] = $item_candidato['contribuicoesFinanceirasPFTotal'] / $item_candidato['contribuicoesFinanceirasPFQtd'];
-  }
-  else {
-    $item_candidato['apoioMedio'] = $item_candidato['contribuicoesFinanceirasPFTotal'];
-  }
+  $json_code = json_encode($candidatos_novo);
 
-  $doacoes = array_values($doacoes);
-
-  usort($doacoes, function ($a, $b) {
-    return $b['valorReceita'] - $a['valorReceita'];
-  });
-
-  $item_candidato['maioresApoiadores'] = array_slice($doacoes, 0, 10);
-  $candidatos_novo[] = $item_candidato;
+  file_put_contents('../' . $arquivo_processado, $json_code);
 }
-
-$json_code = json_encode($candidatos_novo);
-
-file_put_contents('../' . $arquivo_processado, $json_code);
 
 ?>
--- src/assets/coffee/app.coffee
+++ src/assets/coffee/app.coffee
@@ -4,37 +4,44 @@
 
 
 @App.controller 'PageController', ($scope, $http) ->
-  # Se quiser clonar o repositório para fazer para outra cidade, altere aqui
-  # e aponte para o JSON em seu repositório
-  url_json_dados = 'https://raw.githubusercontent.com/' +
-    'pedrorocha-net/eleicoes2016-rj-comparadoacoes' +
-    '/master/data/candidatos_dados_processados.json'
-  $http.get(url_json_dados).then (result) ->
-    $scope.candidatos = result.data
-    $scope.selecao1 = null
-    $scope.selecao2 = null
-    $scope.selecao3 = null
+  base_url = 'https://raw.githubusercontent.com/'
+  repo_url = 'pedrorocha-net/eleicoes2016-rj-comparadoacoes'
+  data_url = "#{base_url}/#{repo_url}/master/data"
 
-    $scope.selecionar = (candidato) ->
-      if $scope.selecao1?.id == candidato.id
-        $scope.selecao1 = null
-        return
-      if $scope.selecao2?.id == candidato.id
-        $scope.selecao2 = null
-        return
-      if $scope.selecao3?.id == candidato.id
-        $scope.selecao3 = null
-        return
+  url_json_config = "#{data_url}/config.json"
 
-      unless $scope?.selecao1
-        $scope.selecao1 = candidato
-        return
-      unless $scope?.selecao1 && $scope?.selecao2
-        $scope.selecao2 = candidato
-        return
-      unless $scope?.selecao1 && $scope?.selecao2 && $scope?.selecao3
-        $scope.selecao3 = candidato
-        return
+  $http.get(url_json_config).then (config) ->
+    $scope.config = result.data
+
+
+  $scope.$watch 'cidade', (newValue, oldValue) ->
+    url_json_dados = "#{data_url}/candidatos_dados_processados.json"
+    $http.get(url_json_dados).then (result) ->
+      $scope.candidatos = result.data
+      $scope.selecao1 = null
+      $scope.selecao2 = null
+      $scope.selecao3 = null
+
+      $scope.selecionar = (candidato) ->
+        if $scope.selecao1?.id == candidato.id
+          $scope.selecao1 = null
+          return
+        if $scope.selecao2?.id == candidato.id
+          $scope.selecao2 = null
+          return
+        if $scope.selecao3?.id == candidato.id
+          $scope.selecao3 = null
+          return
+
+        unless $scope?.selecao1
+          $scope.selecao1 = candidato
+          return
+        unless $scope?.selecao1 && $scope?.selecao2
+          $scope.selecao2 = candidato
+          return
+        unless $scope?.selecao1 && $scope?.selecao2 && $scope?.selecao3
+          $scope.selecao3 = candidato
+          return
 
 
 @App.directive 'card', () ->
